d77137873f6fa4c986d46aea8eed0ea6c7ea075a
DaniyalMShehzad <86773620+DaniyalMShehzad@users.noreply.github.com>
2021-11-04 11:07:42 -0700

    Update dropdown.js

--- src/slider/dropdown.js
+++ src/slider/dropdown.js
@@ -80,7 +80,52 @@ export default function MultipleSelect() {
       typeof value === 'string' ? value.split(',') : value,
     );
   };
+// import React from 'react';
+// import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+// export default class Example extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.toggle = this.toggle.bind(this);
+//     this.onMouseEnter = this.onMouseEnter.bind(this);
+//     this.onMouseLeave = this.onMouseLeave.bind(this);
+//     this.state = {
+//       dropdownOpen: false
+//     };
+//   }
+
+//   toggle() {
+//     this.setState(prevState => ({
+//       dropdownOpen: !prevState.dropdownOpen
+//     }));
+//   }
+
+//   onMouseEnter() {
+//     this.setState({dropdownOpen: true});
+//   }
+
+//   onMouseLeave() {
+//     this.setState({dropdownOpen: false});
+//   }
+
+//   render() {
+//     return (
+//       <Dropdown className="d-inline-block" onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+//         <DropdownToggle caret>
+//           Dropdown
+//         </DropdownToggle>
+//         <DropdownMenu>
+//           <DropdownItem header>Header</DropdownItem>
+//           <DropdownItem disabled>Action</DropdownItem>
+//           <DropdownItem>Another Action</DropdownItem>
+//           <DropdownItem divider />
+//           <DropdownItem>Another Action</DropdownItem>
+//         </DropdownMenu>
+//       </Dropdown>
+//     );
+//   }
+// }
   return (
     <div className="hover">
         <InputLabel id="demo-multiple-name-label">Consumers Electronics</InputLabel>
